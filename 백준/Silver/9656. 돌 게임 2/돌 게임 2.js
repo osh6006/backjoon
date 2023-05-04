8e3c181d79cf809f3ab47fcd53abcef155bf44f3
HSWEB <56256924+osh6006@users.noreply.github.com>
2023-05-04 14:23:28 +0900

    [Silver IV] Title: 돌 게임 2, Time: 196 ms, Memory: 9376 KB -BaekjoonHub

--- 백준/Silver/9656. 돌 게임 2/돌 게임 2.js
+++ 백준/Silver/9656. 돌 게임 2/돌 게임 2.js
@@ -1,18 +1,3 @@
-const fs = require("fs");
-const filePath = process.platform === "linux" ? "/dev/stdin" : "test.txt";
-const input = fs.readFileSync(filePath).toString();
-
-let count = +input;
-let winner = true;
-
-while (count > 0) {
-  if (count > 3) {
-    count -= 3;
-    winner = !winner;
-  } else {
-    count -= 1;
-    winner = !winner;
-  }
-}
-
-console.log(`${winner ? "SK" : "CY"}`);
+const input = +require("fs").readFileSync("/dev/stdin").toString().trim();
+if (input % 2 === 0) console.log("SK");
+else console.log("CY");
