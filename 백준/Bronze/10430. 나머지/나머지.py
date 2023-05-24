6670c7ae3a65dadb36f477e0176fcc6825653bc3
HSWEB <56256924+osh6006@users.noreply.github.com>
2023-05-24 12:16:54 +0900

    [Bronze V] Title: 나머지, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.py
@@ -0,0 +1,12 @@
+import sys
+input = list(map(int, sys.stdin.read().split(" ")))
+
+# 첫째 줄에 (A+B)%C, 둘째 줄에 ((A%C) + (B%C))%C, 셋째 줄에 (A×B)%C, 넷째 줄에 ((A%C) × (B%C))%C 를 출력한다.
+A = input[0]
+B = input[1]
+C = input[2]
+
+print((A+B)%C)
+print(((A%C) + (B%C))%C)
+print((A*B)%C)
+print(((A%C) * (B%C))%C)
