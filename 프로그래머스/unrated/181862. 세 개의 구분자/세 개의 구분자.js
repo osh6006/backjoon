e1d80bfc075bd1bf435c9d70e697acf8b694aad5
HSWEB <56256924+osh6006@users.noreply.github.com>
2023-06-18 23:37:37 +0900

    [unrated] Title: 세 개의 구분자, Time: 10.16 ms, Memory: 52.9 MB -BaekjoonHub

--- 프로그래머스/unrated/181862. 세 개의 구분자/세 개의 구분자.js
+++ 프로그래머스/unrated/181862. 세 개의 구분자/세 개의 구분자.js
@@ -1,25 +1,2 @@
-function solution(myStr) {
-    const op = "abc";
-    let answer = [];
-    let temp = "";
-    
-    for(let i = 0; i<myStr.length; i++){
-        if(op.includes(myStr[i])){
-            answer.push(temp);
-            temp = "";
-        }else{
-            temp += myStr[i];
-        }
-    }
-    
-    answer.push(temp);
-    answer = answer.filter(el=>el!=="");
-    
-    if(answer.length>0){
-        return answer;
-    }else{
-        return ["EMPTY"];
-    }
-    
-    
-}
+
+const solution=s=>s.match(/[^a-c]+/g)||['EMPTY']
