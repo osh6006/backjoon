ad73ff0cfa3a50f0ba88f4e408a946781a8f229d
HSWEB <56256924+osh6006@users.noreply.github.com>
2023-10-19 11:41:39 +0900

    [level 1] Title: 두 정수 사이의 합, Time: 0.13 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.js
+++ 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.js
@@ -1,14 +1,16 @@
+// 다시 안 풀어도 됨 성능 개선을 하려면 아마 dp로풀어야 할듯?
 function solution(a, b) {
-    const answer = []
+    let answer = 0;
+    
     if(a<b){
         for(let i = a; i<=b; i++){
-            answer.push(i);
+            answer+=i;
         }
     }else{
         for(let i = b; i<=a; i++){
-            answer.push(i);
+            answer+=i;
         }
     }
     
-    return answer.reduce((prev,curr)=> prev+=curr);
+    return answer;
 }
