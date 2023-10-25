ff42a8625698eccc91d1fcb513267485350adcb4
HSWEB <56256924+osh6006@users.noreply.github.com>
2023-10-26 00:31:04 +0900

    [level 1] Title: 크기가 작은 부분문자열, Time: 0.22 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.js
+++ 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.js
@@ -1,16 +1,15 @@
+// 다시 안풀어도 됨
 function solution(t, p) {
     const temp = [];
+    let answer = 0;
     
     t.split("").forEach((el,i)=>{
         const substr = t.substring(i, i+p.length);
-        if(substr.length === p.length){
-            temp.push(+substr);
+        if(substr.length === p.length && +substr <= +p){
+            answer++;
         }
     });
     
-    return temp.reduce((prev,curr)=>{
-       if(curr <= +p ) prev++;
-       return prev;
-    },0)
+    return answer;
 }
 
