6d38b1336009c74a4eff25a93248006f32fda136
HSWEB <56256924+osh6006@users.noreply.github.com>
2023-11-11 18:07:53 +0900

    [level 1] Title: 모의고사, Time: 1.55 ms, Memory: 35.8 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.js
+++ 프로그래머스/1/42840. 모의고사/모의고사.js
@@ -1,46 +1,30 @@
 function solution(answers) {
-    let temp = [
-        {index:1,value:0},
-        {index:2,value:0},
-        {index:3,value:0},
-    ]
-    const no1 = [1,2,3,4,5];
-    const no2 = [2, 1, 2, 3, 2, 4, 2, 5];
-    const no3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
+    const a = [1, 2, 3, 4, 5];
+    const b = [2, 1, 2, 3, 2, 4, 2, 5];
+    const c = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
     
-    let index1=0;
-    let index2=0;
-    let index3=0;
+    const answer = [{idx:1,count:0},{idx:2,count:0},{idx:3,count:0}];
     
-    let count1=0;
-    let count2=0;
-    let count3=0;
-    
-   answers.forEach((el,i)=>{
-        if(index1>no1.length-1) index1 = 0;
-        if(index2>no2.length-1) index2 = 0;
-        if(index3>no3.length-1) index3 = 0;
-       
-        if(no1[index1] === el){
-            temp[0] = {index:1, value: temp[0].value + 1 }
+    answers.forEach((el,i)=>{
+        if(el === a[i%a.length]){
+            answer[0].count++;
         }
-       
-       if(no2[index2] === el){
-            temp[1] = {index:2, value: temp[1].value+1 }
+        if(el === b[i%b.length]){
+            answer[1].count++;
+            
         }
-       
-       if(no3[index3] === el){
-            temp[2] = {index:3, value: temp[2].value+1 }
+        if(el === c[i%c.length]){
+            answer[2].count++;
         }
-        
-        index1++;
-        index2++;
-        index3++;
-        
     });
+    const max = answer.reduce((prev,curr)=>{
+        if(prev<curr.count) prev = curr.count;
+        return prev;
+    },0);
     
-    temp.sort((a,b) => b.value - a.value);
-    const max = Math.max(...temp.map(obj => obj.value));
-    return temp.filter((el)=>el.value === max).map(el=>el.index);
-    
+
+    return answer.filter(el=>el.count!==0)
+        .sort((a,b)=>a.count-b.count)
+        .filter((el)=>max === el.count)
+        .map(el=>el.idx);
 }
