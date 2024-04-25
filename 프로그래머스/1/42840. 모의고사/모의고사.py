dc818926708925e474a976dee785715db61c6155
HSWEB <56256924+osh6006@users.noreply.github.com>
2024-04-25 13:52:10 +0900

    [level 1] Title: 모의고사, Time: 9.75 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -0,0 +1,28 @@
+def solution(answers):
+    
+    patterns = [
+        [1,2,3,4,5],
+        [2,1,2,3,2,4,2,5],
+        [3,3,1,1,2,2,4,4,5,5]
+    ]
+    
+    scores = [0]*3
+    
+    for i, answer in enumerate(answers):
+        for j, pattern in enumerate(patterns):
+#             중요한 부분
+            if answer == pattern[i% len(pattern)]:
+                scores[j]+=1
+    
+    max_score = max(scores)
+    
+    print(scores)
+    
+    highest_scores = []
+    
+    for i , score in enumerate(scores):
+        if score == max_score:
+            highest_scores.append(i+1)
+            
+    return highest_scores
+    
